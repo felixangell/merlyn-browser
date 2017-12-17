1fb732b1b813de143c7637a924ca139244c54e5c
Felix Angell <hpuxus+gh@github.com>
2017-12-17 16:40:22 +0000

    frontend using the strife library

---
+++ frontend/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/felixangell/strife"
+)
+
+func main() {
+	window := strife.SetupRenderWindow(1280, 720, strife.DefaultConfig())
+	window.SetTitle("Merlyn")
+	window.SetResizable(true)
+	window.Create()
+
+	window.HandleEvents(func(evt strife.StrifeEvent) {
+		switch event := evt.(type) {
+		case *strife.CloseEvent:
+			println("closing window!")
+			window.Close()
+		case *strife.WindowResizeEvent:
+			println("resize to ", event.Width, "x", event.Height)
+		}
+	})
+
+	ctx := window.GetRenderContext()
+	ctx.Clear()
+	{
+		ctx.SetColor(strife.Red)
+		ctx.Rect(10, 10, 50, 50, strife.Fill)
+	}
+	ctx.Display()
+
+	for {
+		window.PollEvents()
+		if window.CloseRequested() {
+			break
+		}
+	}
+}
